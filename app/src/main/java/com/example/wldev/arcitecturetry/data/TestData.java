fceb8fceb8c7852e3f6d98652e04bca923793bce
Ekalips <djqrj96@gmail.com>
2017-08-04 16:20:12 +0300

    Removed unused thingy

--- app/src/main/java/com/example/wldev/arcitecturetry/data/TestData.java
+++
@@ -1,8 +0,0 @@
-package com.ekalips.wldev.arcitecturetry.data;
-
-/**
- * Created by wldev on 8/1/17.
- */
-
-public class TestData {
-}
--- app/src/main/java/com/example/wldev/arcitecturetry/recycler_view_stuff/annotations/DataSetType.java
+++
@@ -1,9 +0,0 @@
-package com.ekalips.wldev.arcitecturetry.recycler_view_stuff.annotations;
-
-/**
- * Created by wldev on 8/1/17.
- */
-
-public @interface DataSetType {
-    Class<?> dataSetType() default Object.class;
-}
